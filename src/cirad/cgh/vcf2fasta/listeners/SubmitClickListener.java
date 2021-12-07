9e468899a1c5d96396b14da34578ce61bd43b869
gkanogiannis <ganoyan@gmail.com>
2021-12-07 12:19:35 -0500

    Update header

--- src/cirad/cgh/vcf2fasta/listeners/SubmitClickListener.java
+++ src/cirad/cgh/vcf2fasta/listeners/SubmitClickListener.java
@@ -1,21 +1,24 @@
-/*******************************************************************************
- * Copyright (C) 2016 <CIRAD>
+/*
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3 as
- * published by the Free Software Foundation.
+ * CGH_VCF2Fasta cirad.cgh.vcf2fasta.listeners.SubmitClickListener
  *
- * This program is distributed in the hope that it will be useful,
+ * Copyright (C) 2021 Anestis Gkanogiannis <anestis@gkanogiannis.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * See <http://www.gnu.org/licenses/gpl-3.0.html> for details about
- * GNU Affero General Public License V3.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
- * Contributors:
- *     Anestis Gkanogiannis <ganoyan@gmail.com>
- *******************************************************************************/
+ */
 package cirad.cgh.vcf2fasta.listeners;
 
 public interface SubmitClickListener<T> {
--- src/cirad/cgh/vcf2fasta/ws/gatk_alternativefasta/AppServicePortType.java
+++ src/cirad/cgh/vcf2fasta/ws/gatk_alternativefasta/AppServicePortType.java
@@ -1,26 +1,23 @@
-/*******************************************************************************
- * Copyright (C) 2016 <CIRAD>
+/*
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3 as
- * published by the Free Software Foundation.
+ * CGH_VCF2Fasta cirad.cgh.vcf2fasta.ws.gatk_alternativefasta.AppServicePortType
  *
- * This program is distributed in the hope that it will be useful,
+ * Copyright (C) 2021 Anestis Gkanogiannis <anestis@gkanogiannis.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * See <http://www.gnu.org/licenses/gpl-3.0.html> for details about
- * GNU Affero General Public License V3.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
- * Contributors:
- *     Anestis Gkanogiannis <ganoyan@gmail.com>
- *******************************************************************************/
-/**
- * AppServicePortType.java
- *
- * This file was auto-generated from WSDL
- * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
  */
 
 package cirad.cgh.vcf2fasta.ws.gatk_alternativefasta;
